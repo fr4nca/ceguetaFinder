1c4a8e58a8e663ebfc630dfd67aab855c04137d0
Victor Almeida França <fr4ncacc@gmail.com>
2019-06-06 00:08:38 -0300

    css file, about page, navbar

--- app/server.js
+++ app/server.js
@@ -1,29 +1,33 @@
-const path = require("path");
-const express = require("express");
-const http = require("http");
+const path = require('path');
+const express = require('express');
+const http = require('http');
 const app = express();
 const server = http.Server(app);
-const io = require("socket.io")(server);
-require("./src/controllers/ioController")(io);
-const hbs = require("express-handlebars");
+const io = require('socket.io')(server);
+require('./src/controllers/ioController')(io);
+const hbs = require('express-handlebars');
 
 // Configs
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "src", "views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'src', 'views'));
 app.engine(
-  "hbs",
+  'hbs',
   hbs({
-    extname: "hbs"
+    extname: 'hbs',
+    partialsDir: path.join(__dirname, 'src', 'views', 'partials')
   })
 );
 
 // Middlewares
-app.use(express.static(path.join(__dirname, "src", "public", "static")));
+app.use(express.static(path.join(__dirname, 'src', 'public', 'static')));
 
 // Routes
-app.use("/api/map", require("./src/routes/api/map"));
-app.get("/", (req, res) => {
-  res.render("index");
+app.use('/api/map', require('./src/routes/api/map'));
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
 const port = process.env.PORT || 3000;
