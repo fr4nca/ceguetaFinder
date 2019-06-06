3bacd2243c6cd1e733bc76b7b8960153e60e24b4
Victor Almeida França <fr4ncacc@gmail.com>
2019-06-06 17:24:00 -0300

    fim

--- app/server.js
+++ app/server.js
@@ -35,6 +35,6 @@ app.get('/', (req, res) => {
   res.render('index', { key: process.env.google_api });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
